079be7f722dc7a079a4d36437d7cfc573160ef2a
Chris McKenzie <kristopolous@yahoo.com>
2015-09-13 16:16:26 -0700

    more python work

--- tube-get.py
+++ tube-get.py
@@ -2,10 +2,35 @@
 
 import sys,os,re
 
+re_generic_url = re.compile('(http[:\s\/\w\.]*(flv|mp4))')
+re_kv_url = re.compile('(?<=file=)(http[:\s\/\w\.]*(flv|mp4))')
+
 if len(sys.argv) > 1:
     os.chdir(sys.argv[1])
 
 while True:
     line = sys.stdin.readline()
     domain = re.search('(http[:\s\/]*[^\/]*)', line).group(1)
-    print domain
+    page = os.popen('curl -s %s' % line).read()
+
+    video = re.search('(http[:\s\/\w\.]*(flv|mp4))[\"]', page)
+    if not video:
+        player_config_url = re.search('([:\/\w\.]*playerConfig.php[^"]*)', page)
+
+        if not player_config_url:
+            video = re_kv_url.search(page)
+            print video
+
+            if not video:
+                video = re_generic_url.search(page)
+                print video
+
+        else:
+
+            video = re_kv_url.search(page)
+            if not video 
+
+        print player_config_url
+
+
+    print video
