b724456ebc037a96e89e805ad3e44277c597f7ac
Chris McKenzie <kristopolous@yahoo.com>
2016-07-01 17:23:36 -0700

    smarter scraping

--- tube-get.py
+++ tube-get.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
-import sys,os,re
+import sys,os,re,pprint
 
 re_generic_url = re.compile('(http[:\s\/\w\.]*(flv|mp4))')
 re_kv_url = re.compile('(?<=file=)(http[:\s\/\w\.]*(flv|mp4))')
+pp = pprint.PrettyPrinter(indent=4)
 
 if len(sys.argv) > 1:
     os.chdir(sys.argv[1])
@@ -27,8 +28,8 @@ while True:
 
     domain = re.search('(http[:\s\/]*[^\/]*)', line).group(1)
     page = os.popen('curl -s %s' % line).read()
-    log(line)
-    video = re.search('(http[:\-\s\/\w\.]*(flv|mp4))[\"\']', page)
+    #log(line)
+    video = re.findall('(http[:\-\s\/\w\.]*(flv|mp4))[\"\'\&]', page)
     if not video:
         log("No mp4 found")
         player_config_url = re.search('([:\/\w\.]*playerConfig.php[^"]*)', page)
@@ -50,10 +51,14 @@ while True:
 
         #print player_config_url
 
+    pp.pprint(video)
     if video:
         video = video.group(0)
 
-    #print video
+
+    if not video:
+        print("Error not found for %s" % source_url)
+        continue
 
     has_domain = re.search('(http[:\s\/]*[^\/]*)', video) 
 
@@ -63,6 +68,7 @@ while True:
     video = video.strip('\'"')
 
     print video
+    """
     options = " ".join([
         '--no-use-server-timestamps',
         '--header="Referer: %s"' % source_url,
@@ -74,4 +80,5 @@ while True:
 
     with open("tube-get.sources-list.txt", "a") as log:
         log.write("%s -> %s\n" %( source_url, video ))
+    """
 
